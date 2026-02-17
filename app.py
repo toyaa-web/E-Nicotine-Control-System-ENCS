5ccdbe427e1d03791386819dd21b8f4ad1f96bc8
toyaa-web <toyaalaa366@gmail.com>
2026-02-17 10:53:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -304,5 +304,10 @@ def home():
         min_age=MINIMUM_AGE
   )
 
-if __name__ == "__main__": app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
